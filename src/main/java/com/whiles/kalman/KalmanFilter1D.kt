aa1f3803e0ad6044d5f20daa6af41105bb52bef4
Shawn Lu <lu.shangchiun@gmail.com>
2020-09-16 15:00:40 +0800

    Updated KalmanFilter1D.kt

--- src/main/java/com/whiles/kalman/KalmanFilter1D.kt
+++ src/main/java/com/whiles/kalman/KalmanFilter1D.kt
@@ -1,4 +1,24 @@
 package com.whiles.kalman
 
-class KalmanFilter1D {
+class KalmanFilter1D(guess: Double, estimateUncertainty: Double, val measureUncertainty: Double) {
+
+    /**
+     * error variance
+     */
+    private var p = estimateUncertainty * estimateUncertainty + measureUncertainty
+    private var r = measureUncertainty * measureUncertainty
+
+    /**
+     * Prediction
+     */
+    private var prediction = guess
+
+    private var K: Double = 0.0
+
+    fun measure(z: Double): Double {
+        K = p / (p + r)
+        prediction = prediction + K * (z - prediction)
+        p = (1 - K) * p + measureUncertainty
+        return prediction
+    }
 }
