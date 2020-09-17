279ab086274cc1523a27714105c3bd52408a6fea
Shawn Lu <lu.shangchiun@gmail.com>
2020-09-17 22:47:33 +0800

    Updated LiveCycleGpsSpeedDataEngine.kt

--- src/main/java/com/whiles/kalman/AlphaBetaFilter.kt
+++ src/main/java/com/whiles/kalman/AlphaBetaFilter.kt
@@ -1,5 +1,7 @@
 package com.whiles.kalman
 
+import android.util.Log
+
 /**
  * This is used to examine a dynamic system that changes its state over the time.
  * For example, we are going to track the constant velocity aircraft in one dimension
@@ -10,15 +12,21 @@ package com.whiles.kalman
  * @param alpha will be between 0 to 1
  * @param beta will be between 0 to 1
  */
-class AlphaBetaFilter(valueGuess: Double, velocityGuess: Double, val alpha: Float, val beta: Float) {
+class AlphaBetaFilter(
+    valueGuess: Double,
+    velocityGuess: Double,
+    val alpha: Float,
+    val beta: Float
+) {
     var range = valueGuess
-    var velocity  = velocityGuess
+    var velocity = velocityGuess
 
-    fun measure(z: Double, interval: Float): Double{
+    fun measure(z: Double, interval: Float): Double {
+
         val prediction = range + velocity * interval
         range = prediction + alpha * (z - prediction)
         velocity += beta * (z - prediction) / interval
-
+        Log.d("AlphaBetaFilter", "measure: z=${String.format("%.1f", z)}, interval=${String.format("%.1f", interval)}, estimate=${String.format("%.1f", range)}")
         return range
     }
 }
